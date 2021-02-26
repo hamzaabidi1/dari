31d902d6a0915d4abef23d058c8981c2edc46ba6
Eya <bentali.aya@gmail.com>
2021-02-27 00:16:21 +0100

    entities

---
+++ src/main/java/tn/Dari/entity/Recherche.java
@@ -0,0 +1,144 @@
+package tn.Dari.entity;
+
+public class Recherche {
+	
+	private int Id;
+	private String region;
+	private String ville;
+	private float minPrice;
+	private float maxPrice;
+	private int nbrChambres;
+	private String gamme;
+	private Status statut;
+	
+	
+	
+	public Recherche() {
+		super();
+	}
+
+
+
+	public Recherche(int id, String region, String ville, float minPrice, float maxPrice, int nbrChambres, String gamme,
+			Status statut) {
+		super();
+		Id = id;
+		this.region = region;
+		this.ville = ville;
+		this.minPrice = minPrice;
+		this.maxPrice = maxPrice;
+		this.nbrChambres = nbrChambres;
+		this.gamme = gamme;
+		this.statut = statut;
+	}
+
+
+
+	public int getId() {
+		return Id;
+	}
+
+
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+
+
+	public String getRegion() {
+		return region;
+	}
+
+
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+
+	public float getMinPrice() {
+		return minPrice;
+	}
+
+
+
+	public void setMinPrice(float minPrice) {
+		this.minPrice = minPrice;
+	}
+
+
+
+	public float getMaxPrice() {
+		return maxPrice;
+	}
+
+
+
+	public void setMaxPrice(float maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+
+
+
+	public int getNbrChambres() {
+		return nbrChambres;
+	}
+
+
+
+	public void setNbrChambres(int nbrChambres) {
+		this.nbrChambres = nbrChambres;
+	}
+
+
+
+	public String getGamme() {
+		return gamme;
+	}
+
+
+
+	public void setGamme(String gamme) {
+		this.gamme = gamme;
+	}
+
+
+
+	public Status getStatut() {
+		return statut;
+	}
+
+
+
+	public void setStatut(Status statut) {
+		this.statut = statut;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Recherche [Id=" + Id + ", region=" + region + ", ville=" + ville + ", minPrice=" + minPrice
+				+ ", maxPrice=" + maxPrice + ", nbrChambres=" + nbrChambres + ", gamme=" + gamme + ", statut=" + statut
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/tn/Dari/entity/Reviews.java
@@ -0,0 +1,114 @@
+package tn.Dari.entity;
+
+public class Reviews {
+	
+	private int idr;
+	private int note1;
+	private int note2;
+	private int note3;
+	private int note4;
+	private int note5;
+	private int note6;
+	
+	
+	public Reviews(int idr, int note1, int note2, int note3, int note4, int note5, int note6) {
+		super();
+		this.idr = idr;
+		this.note1 = note1;
+		this.note2 = note2;
+		this.note3 = note3;
+		this.note4 = note4;
+		this.note5 = note5;
+		this.note6 = note6;
+	}
+
+
+	public Reviews() {
+		super();
+	}
+
+
+	public int getIdr() {
+		return idr;
+	}
+
+
+	public void setIdr(int idr) {
+		this.idr = idr;
+	}
+
+
+	public int getNote1() {
+		return note1;
+	}
+
+
+	public void setNote1(int note1) {
+		this.note1 = note1;
+	}
+
+
+	public int getNote2() {
+		return note2;
+	}
+
+
+	public void setNote2(int note2) {
+		this.note2 = note2;
+	}
+
+
+	public int getNote3() {
+		return note3;
+	}
+
+
+	public void setNote3(int note3) {
+		this.note3 = note3;
+	}
+
+
+	public int getNote4() {
+		return note4;
+	}
+
+
+	public void setNote4(int note4) {
+		this.note4 = note4;
+	}
+
+
+	public int getNote5() {
+		return note5;
+	}
+
+
+	public void setNote5(int note5) {
+		this.note5 = note5;
+	}
+
+
+	public int getNote6() {
+		return note6;
+	}
+
+
+	public void setNote6(int note6) {
+		this.note6 = note6;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reviews [idr=" + idr + ", note1=" + note1 + ", note2=" + note2 + ", note3=" + note3 + ", note4=" + note4
+				+ ", note5=" + note5 + ", note6=" + note6 + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
