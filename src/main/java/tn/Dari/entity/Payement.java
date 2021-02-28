d7673b4cf1e661b5e7da1b09a7b49cd6f31b7c44
hamzaabidi1 <hamza.abidi1@esprit.tn>
2021-03-01 00:08:07 +0100

    other

--- src/main/java/tn/Dari/entity/Payement.java
+++ src/main/java/tn/Dari/entity/Payement.java
@@ -4,5 +4,29 @@ public class Payement {
 	
 	private int id;
 	private String justification;
+	public Payement(int id, String justification) {
+		super();
+		this.id = id;
+		this.justification = justification;
+	}
+	
+	public Payement() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getJustification() {
+		return justification;
+	}
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+	
+	
 
 }
